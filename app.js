144664c79f60f8481294160d201c72a3c1693f47
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-06-19 02:07:20 +0530

    next

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app=express();
 const studentRouter = require('./api/routes/student');
 const facultyRouter = require('./api/routes/faculty');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use('/student',studentRouter);
 app.use('/faculty',facultyRouter);
+app.use((req,res,next)=>
+{ 
+    // 404 for if in case url not found
+    // 4040 will display error
+    res.status(404).json({
+        error:"bad request"
+    })
+})
+
 // this is how to connect databse
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://anjaliraj41095:Anjali1234@cluster0.elh10cx.mongodb.net/?retryWrites=true&w=majority')
@@ -13,14 +25,8 @@ mongoose.connection.on('error',err=>{
 mongoose.connection.on('connected',connected=>{
     console.log("database connected");
 });
-app.use((req,res,next)=>
-{ 
-    // 404 for if in case url not found
-    // 4040 will display error
-    res.status(404).json({
-        error:"bad request"
-    })
-})
+
+
 // app.use((req, res, next) => {
 //     res.status(200).json({
 //         message: 'app is running'
